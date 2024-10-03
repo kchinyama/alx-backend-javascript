25c6b4181258e94c037d76446479eb339aa28259
Kaumba Chinyama <kaumba@kalichipictures.com>
2024-10-03 08:45:19 -0700

    default parameters

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,3 +1,5 @@
+// default parameters code sample
+
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
